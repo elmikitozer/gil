1e193ffd2271682ab546e276747ec58db1851e92
Mikaya <mikayay@icloud.com>
2025-08-11 01:00:01 +0200

    first commit w a lot of code lol mb

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import { getStoryblokApi, StoryblokServerComponent } from "@storyblok/react/rsc";
 import { ensureStoryblok } from "@/lib/storyblok";
-import { KNOWN } from "@/lib/storyblok";
-import Unknown from "@/lib/unkown";
+// import { KNOWN } from "@/lib/storyblok";
+// import Unknown from "@/lib/unkown";
 // import HeroLogo from "./components/HeroLogo";
 
 
@@ -10,7 +10,10 @@ export default async function Home() {
   ensureStoryblok();
   const api = getStoryblokApi();
   
-  const version = process.env.NODE_ENV === "production" ? "published" : "draft";
+  const version: "published" | "draft" =
+  process.env.NODE_ENV === "production" ? "published" : "draft";
+
+
   const { data } = await api.get("cdn/stories/home", { version });
   return (
     <>
--- next.config.ts
+++ next.config.ts
@@ -1,19 +1,26 @@
+// next.config.ts
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "a.storyblok.com",
-      },
-      // (facultatif) d'autres CDN Storyblok si tu en rencontres
-      // { protocol: "https", hostname: "img2.storyblok.com" },
+      { protocol: "https", hostname: "a.storyblok.com" }, // images Storyblok
     ],
+  },
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          { key: "X-Content-Type-Options", value: "nosniff" },
+          { key: "Referrer-Policy", value: "strict-origin-when-cross-origin" },
+          { key: "Permissions-Policy", value: "camera=(), microphone=(), geolocation=()" },
+          // Active uniquement si ton domaine est bien en HTTPS:
+          { key: "Strict-Transport-Security", value: "max-age=63072000; includeSubDomains; preload" },
+        ],
+      },
+    ];
   },
 };
 
-
-
 export default nextConfig;
-
